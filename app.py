7d58d7f75666cec5ba109b52a50ebd78b249ba2a
anonymousfraand <praveenrijal1980@gmail.com>
2022-11-23 22:54:30 +0530

    Lol

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import requests
+import json
+
+
+API_KEY = "0zRs_HVV9HNSSzHy-sO_y_XWgsYQFlchHxmeakje1g0H"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+app = Flask(__name__, static_url_path='')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/checkEligibility')
+def checkEligibility():
+    return render_template('Demo2.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    greScore = int(request.form['greScore'])
+    toeflScore = int(request.form['toeflScore'])
+    univRank = int(request.form['univRank'])
+    sop = float(request.form['sop'])
+    lor = float(request.form['lor'])
+    cgpa = float(request.form['cgpa'])
+    research = int(request.form['research'])
+    array_of_input_fields = ['greScore', 'toeflScore', 'univRank', 'sop', 'lor', 'cgpa', 'research']
+    array_of_values_to_be_scored = [greScore, toeflScore, univRank, sop, lor, cgpa, research]
+    payload_scoring = {"input_data": [{"field": [array_of_input_fields], "values": [array_of_values_to_be_scored]}]}
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/3b55c9e9-2973-429f-b8e0-d69b19f4ede6/predictions?version=2022-11-12', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+    predictions = response_scoring.json()
+    prediction = predictions['predictions'][0]['values'][0][0]
+    
+
+    if (int(prediction[0]*100)>0):
+        return render_template('chance.html')
+    else:
+        return render_template('noChance.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
